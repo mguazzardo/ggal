b38cc78760b394e53d6c35a4deaf883895b78b1e
mguazzardo <mguazzardo76@gmail.com>
2020-08-01 03:21:49 +0000

    subiendo ficheros

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>Examen banco galicia</title>
+</head>
+<body>
+<img src="logo-galicia.png" border="1" alt="Este es el ejemplo de un texto alternativo">
+<h1>Hola mundo banco galicia</h1>
+</body>
+</html>
