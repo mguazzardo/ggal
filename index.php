96ce9a64da34390b7aa2da1566b2b8b5535ba178
mguazzardo <mguazzardo76@gmail.com>
2020-08-01 04:57:48 +0000

    curl_test

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 </head>
 <body>
 <img src="logo-galicia.png" border="1" alt="Este es el ejemplo de un texto alternativo">
-<h1>Hola mundo banco galicia</h1>
+<h1>Esta es una aplicacion realizada para examen de seleccion de Banco Galicia</h1>
 </body>
 </html>
